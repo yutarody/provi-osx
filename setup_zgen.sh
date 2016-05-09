c700b4d4737943adac02aec42b0a7e9e161e9d1d
Yutaro Horikawa <yutaro.ver05+github@gmail.com>
2016-05-09 17:32:16 +0900

    change flow

--- setup_zgen.sh
+++ setup_zgen.sh
@@ -9,19 +9,21 @@ brew install stow
 echo '/usr/local/bin/zsh' | pbcopy
 #insert to shells
 sudo vim /etc/shells
+#switch homebrew zsh
 chsh -s /usr/local/bin/zsh
 
-#zgen clone
-git clone git@github.com:tarjoilija/zgen.git
-#important!!!must replace my init.zsh
+#reload .zshrc
+exec zsh
+
+#dotfiles clone
 git clone https://github.com/yutarody/dotfiles.git
-ln -nsf $HOME/dotfiles/init.zsh $HOME/.zgen/init.zsh
-ln -nsf $HOME/dotfiles/001-myset $HOME/.zshrc.d/
 
 #Set up Zgen and the starter kit
 git clone git@github.com:unixorn/zsh-quickstart-kit.git
 cd zsh-quickstart-kit
 stow --target=$HOME zsh
+ln -nsf $HOME/dotfiles/001-myset $HOME/.zshrc.d/
 
-#reboot shell
+#reload .zshrc
 exec $SHELL -l
+exec zsh
