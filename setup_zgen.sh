840dd134dec0811bb8a43de4d1d1ee76a1d6b695
Yutaro Horikawa <yutaro.ver05+github@gmail.com>
2016-06-23 15:42:38 +0900

    Update setup_zgen.sh

--- setup_zgen.sh
+++ setup_zgen.sh
@@ -2,27 +2,29 @@
 
 #Homebrew setup
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh
+
+#dotfiles clone
+git clone https://github.com/yutarody/dotfiles.git
+
+#zgen clone
+git clone git@github.com:tarjoilija/zgen.git
+
+#Set up Zgen and the starter kit
+zsh
 brew install stow
+git clone git@github.com:unixorn/zsh-quickstart-kit.git
+cd zsh-quickstart-kit
+stow --target=$HOME zsh
+mkdir $HOME/.zshrc.d
+ln -nsf $HOME/dotfiles/001-myset $HOME/.zshrc.d/
 
 #Switch Homebrew zsh
+brew install --without-etcdir zsh
 echo '/usr/local/bin/zsh' | pbcopy
 #insert to shells
 sudo vim /etc/shells
 #switch homebrew zsh
 chsh -s /usr/local/bin/zsh
-
-#reload .zshrc
-exec zsh
-
-#dotfiles clone
-git clone https://github.com/yutarody/dotfiles.git
-
-#Set up Zgen and the starter kit
-git clone git@github.com:unixorn/zsh-quickstart-kit.git
-cd zsh-quickstart-kit
-stow --target=$HOME zsh
-ln -nsf $HOME/dotfiles/001-myset $HOME/.zshrc.d/
 
 #reload .zshrc
 exec $SHELL -l
